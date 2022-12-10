533e9e6531d3bb0366c91cc857ebad321b8d0a86
Fabio Ramponi <fabio.ramponi@gmail.com>
2022-12-10 02:02:07 +0100

    aoc 2022 - day 08

--- 2022/days/src/lib.rs
+++ 2022/days/src/lib.rs
@@ -7,6 +7,7 @@ pub mod day_04;
 pub mod day_05;
 pub mod day_06;
 pub mod day_07;
+pub mod day_08;
 
 pub trait DayChallenge {
     fn part_1(&mut self) -> String;
@@ -22,6 +23,7 @@ pub fn get_day(day: u8, purp: Purpose) -> Option<Box<dyn DayChallenge>> {
         5 => Some(Box::new(day_05::Day05::init(purp))),
         6 => Some(Box::new(day_06::Day06::init(purp))),
         7 => Some(Box::new(day_07::Day07::init(purp))),
+        8 => Some(Box::new(day_08::Day08::init(purp))),
         _ => None,
     }
 }
